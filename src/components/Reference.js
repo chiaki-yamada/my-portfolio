16a2d54192bb44700d0fe97318fb59f6ce610be9
chiaki-yamada <110213744+chiaki-yamada@users.noreply.github.com>
2023-05-16 17:09:13 -0700

    updated js

--- src/components/Reference.js
+++ src/components/Reference.js
@@ -17,7 +17,7 @@ function Reference() {
           <div className='bio'>
             <h3 className='bio-name'>Daniel Campbel</h3>
             <h4 className='bio-title'>Web Developer/Artist</h4>
-            <p className='bio-text'>"Working with Chiaki was a pleasure. She learned new skills very quickly and was always a joy to collaborate with. I'd work with her anytime."</p>
+            <p className='bio-text'>“Working with Chiaki was a pleasure. She learned new skills very quickly and was always a joy to collaborate with. I'd work with her anytime.”</p>
           </div>
         </div>
 
@@ -39,7 +39,7 @@ function Reference() {
           <div className='bio'>
             <h3 className='bio-name'>Mika Embury</h3>
             <h4 className='bio-title'>VJLS-JH<br></br>People and Culture Coordinator</h4>
-            <p className='bio-text'>"I loved working with Chiaki! She is hardworking, reliable, and fun. She brings her unique, international, multicultural experience to the workplace, greatly benefiting our organizational culture. Chiaki was also key in developing and launching our new website, bringing a ton of positive attention to our organization and programs!"</p>
+            <p className='bio-text'>“I loved working with Chiaki! She is hardworking, reliable, and fun. She brings her unique, international, multicultural experience to the workplace, greatly benefiting our organizational culture. Chiaki was also key in developing and launching our new website, bringing a ton of positive attention to our organization and programs!”</p>
           </div>
         </div>
 
@@ -50,7 +50,7 @@ function Reference() {
           <div className='bio'>
             <h3 className='bio-name'>Kanei Ou</h3>
             <h4 className='bio-title'>VJLS-JH<br></br>Accounting Coordinator</h4>
-            <p className='bio-text'>“I worked with Chiaki for about a year and I witnessed her single-handedly launching our organization's new website. Through the project, she demonstrated her good sense of color and strong communication skills with third-party developers. She's easy to talk to and a true team player. “</p>
+            <p className='bio-text'>“I worked with Chiaki for about a year and I witnessed her single-handedly launching our organization's new website. Through the project, she demonstrated her good sense of color and strong communication skills with third-party developers. She's easy to talk to and a true team player.”</p>
           </div>
         </div>
 
