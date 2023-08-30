3e8368f7e97b998d8e0b8f7fc689616f12d0fc39
chiaki-yamada <110213744+chiaki-yamada@users.noreply.github.com>
2023-08-29 17:19:24 -0700

    updated css

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,9 @@ const Header = () => {
       <nav>
         <ul>
           <li><NavLink to="/" end className="active"><img src={logo} className='icon' alt='logo' /></NavLink></li>
-          <li><NavLink to="/about" className="active">ABOUT</NavLink></li>
-          <li><NavLink to="/works" className="active">WORKS</NavLink></li>
-          <li><NavLink to="/reference" className="active">REFERENCE</NavLink></li>
+          <li><NavLink to="/about" className="active nav-link">ABOUT</NavLink></li>
+          <li><NavLink to="/works" className="active nav-link">WORKS</NavLink></li>
+          <li><NavLink to="/reference" className="active nav-link">REFERENCE</NavLink></li>
         </ul>
       </nav>
 
